1f8654866a6dcf791f521b8b59c9ea29da698190
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-02 10:55:46 +0700

    menambahkan fungsi input untuk array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -6,3 +6,30 @@ int arr[20], B[20];
 // n is array input size
 int n;
 
+void input ()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n<= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n-------------------" << endl;
+    cout << "\nInputkan isi Element Array" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "array index ke-" << i << " : ";
+        cin >> arr[i];
+    }
+}
+
